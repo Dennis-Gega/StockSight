957b19900e8d384a71aac4dcde88f1b8e277cdfd
Parth Sachdev <parth.sachdev@uconn.edu>
2025-11-24 17:38:44 -0500

    bjdbkaj
    mbak

--- frontend/src/Results.jsx
+++ frontend/src/Results.jsx
@@ -109,7 +109,7 @@ export default function Results() {
           rsi: latestRsi,
           macd: latestMacd,
         }}
-        showTooltips={true}
+        showTooltips={true} // pass flag for tooltips
       />
 
       <PriceChart data={chartData} showBB={toggles.bb} />
